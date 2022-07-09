55fc4b46bf21a2f81cb1032b1e5b019a86c53d79
KayoVinicius <kayovinicius227@gmail.com>
2022-07-09 13:09:35 -0300

    Adição de perguntas no Banco de Dados

--- quiz/lib/quizDados.dart
+++ quiz/lib/quizDados.dart
@@ -1,86 +1,154 @@
- List quiz = [
-      {
-        "Pergunta": "Quem descobriu o Brasil?",
-        "Respostas": [
-          "Dom Pedro I",
-          "Pedro Alvares Cabral",
-          "Tiradentes",
-          "Dom Predro II"
-        ],
-        "Alternativas_Corretas": 2,
-      },
-  
-    {
-      "Pergunta": "O que é Flutter?",
-      "Respostas": [
-        "Uma linguagem de programção",
-        "Um aplicativo",
-        "Um SDK",
-        "Um notebook"
-      ],
-      "Alternativas_Corretas": 3,
-    },
-    
-    {
-      "Pergunta": "O Brasil faz parte de qual bloco econômico?",
-      "Respostas": ["Mercosul", "CEI", "NAFTA", "União Europeia"],
-      "Alternativas_Corretas": 1,
-    },
-    
-    {
-      "Pergunta": "Normalmente, quantos litros de sangue uma pessoa tem?",
-      "Respostas": [
-        "Tem entre 2 a 4 litros",
-        "Tem entre 4 a 6 litros",
-        "Tem 10 litros",
-        "Tem 7 litros"
-      ],
-      "Alternativas_Corretas": 2,
-    },
+List quiz = [
+  {
+    "Pergunta": "Quem descobriu o Brasil?",
+    "Respostas": [
+      "Dom Pedro I",
+      "Pedro Alvares Cabral",
+      "Tiradentes",
+      "Dom Predro II"
+    ],
+    "Alternativas_Corretas": 2,
+  },
+  {
+    "Pergunta": "O que é Flutter?",
+    "Respostas": [
+      "Uma linguagem de programção",
+      "Um aplicativo",
+      "Um SDK",
+      "Um notebook"
+    ],
+    "Alternativas_Corretas": 3,
+  },
+  {
+    "Pergunta": "O Brasil faz parte de qual bloco econômico?",
+    "Respostas": ["Mercosul", "CEI", "NAFTA", "União Europeia"],
+    "Alternativas_Corretas": 1,
+  },
+  {
+    "Pergunta": "Normalmente, quantos litros de sangue uma pessoa tem?",
+    "Respostas": [
+      "Tem entre 2 a 4 litros",
+      "Tem entre 4 a 6 litros",
+      "Tem 10 litros",
+      "Tem 7 litros"
+    ],
+    "Alternativas_Corretas": 2,
+  },
+  {
+    "Pergunta": "De quem é a famosa frase “Penso, logo existo”?",
+    "Respostas": ["Platão", "Galileu Galilei", "Descartes", "Sócrates"],
+    "Alternativas_Corretas": 3,
+  },
+  {
+    "Pergunta": "Quais o menor e o maior país do mundo?",
+    "Respostas": [
+      "Vaticano e Rússia",
+      "Nauru e China",
+      "Mônaco e Canadá",
+      "São Marino e Índia"
+    ],
+    "Alternativas_Corretas": 1,
+  },
+  {
+    "Pergunta": "Quantas casas decimais tem o número pi?",
+    "Respostas": ["Milhares", "Vinte", "Centenas", "Infinitas"],
+    "Alternativas_Corretas": 4,
+  },
+  {
+    "Pergunta": "Quanto tempo a luz do Sol demora para chegar à Terra?",
+    "Respostas": ["12 minutos", "1 dia", "8 minutos", "segundos"],
+    "Alternativas_Corretas": 3,
+  },
+  {
+    "Pergunta": "Em que período da pré-história o fogo foi descoberto?",
+    "Respostas": [
+      "Paleolítico",
+      "Idade dos Metais",
+      "Neolítico",
+      "Período da Pedra Polida"
+    ],
+    "Alternativas_Corretas": 1,
+  },
+  {
+    "Pergunta": "Em qual local da Ásia o português é língua oficial?",
+    "Respostas": ["Índia", "Macau", "Filipinas", "Moçambique"],
+    "Alternativas_Corretas": 2,
+  },
+  {
+    "Pergunta": "Qual é o plural de chapéu?",
+    "Respostas": ["chapéus", "chapeletas", "cartolas", "chapéis"],
+    "Alternativas_Corretas": 1,
+  },
+  {
+    "Pergunta": "Compatriota é aquele indivíduo que...",
+    "Respostas": [
+      "tem parentes no exterior",
+      "ama a pátria acima de tudo",
+      "se origina da mesma terra que outro",
+      "realiza compras em diferentes países"
+    ],
+    "Alternativas_Corretas": 3,
+  },
+  {
+    "Pergunta": 'Qual dos advérbios abaixo não existe?',
+    "Respostas": ["bastante", "quão", "menas", "tanto"],
+    "Alternativas_Corretas": 3,
+  },
+  {
+    "Pergunta": "Uma pessoa eclética é aquela que...",
+    "Respostas": [
+      "aprecia diferentes estilos, métodos, etc",
+      "gosta de apenas um tipo de música",
+      "viajou para vários países",
+      "tem medo de elevador"
+    ],
+    "Alternativas_Corretas": 1,
+  },
+  {
+    "Pergunta": "Quantas sílabas tem a palavra fisioterapeuta?",
+    "Respostas": ["cinco", "seis", "sete", "oito"],
+    "Alternativas_Corretas": 3,
+  },
+  {
+    "Pergunta":
+        'A primeira frase do hino brasileiro diz: "Ouviram do Ipiranga às margens plácidas". O que significa "plácidas"?',
+    "Respostas": ["frias", "calmas", "fundas", "rasas"],
+    "Alternativas_Corretas": 2,
+  },
+  {
+    "Pergunta": 'A palavra "volúpia" está relacionada com:',
+    "Respostas": ["luxúria", "doença", "pobreza", "nervosismo"],
+    "Alternativas_Corretas": 1,
+  },
+  {
+    "Pergunta": 'O verbo "aferir" está relacionado a:',
+    "Respostas": ["capacitar", "medir", "machucar", "manchar"],
+    "Alternativas_Corretas": 2,
+  },
+  {
+    "Pergunta": 'A palavra "latifúndio" está relacionada com:',
+    "Respostas": [
+      "coordenadas geográficas de um local",
+      "abrigo para cães",
+      "produtos de derivados do leite",
+      "grande área rural"
+    ],
+    "Alternativas_Corretas": 4,
+  },
+  {
+    "Pergunta": 'Incumbência significa...',
+    "Respostas": [
+      "descascar maçãs",
+      "conferir responsabilidade a alguém",
+      "interferir nos problemas dos outros",
+      "uma doença pulmonar"
+    ],
+    "Alternativas_Corretas": 2,
+  },
 
-    {
-      "Pergunta": "De quem é a famosa frase “Penso, logo existo”?",
-      "Respostas": ["Platão", "Galileu Galilei", "Descartes", "Sócrates"],
-      "Alternativas_Corretas": 3,
-    },
-    
-    {
-      "Pergunta": "Quais o menor e o maior país do mundo?",
-      "Respostas": [
-        "Vaticano e Rússia",
-        "Nauru e China",
-        "Mônaco e Canadá",
-        "São Marino e Índia"
-      ],
-      "Alternativas_Corretas": 1,
-    },
-    
-    {
-      "Pergunta": "Quantas casas decimais tem o número pi?",
-      "Respostas": ["Milhares", "Vinte", "Centenas", "Infinitas"],
-      "Alternativas_Corretas": 4,
-    },
-    
-    {
-      "Pergunta": "Quanto tempo a luz do Sol demora para chegar à Terra?",
-      "Respostas": ["12 minutos", "1 dia", "8 minutos", "segundos"],
-      "Alternativas_Corretas": 3,
-    },
-    
-    {
-      "Pergunta": "Em que período da pré-história o fogo foi descoberto?",
-      "Respostas": [
-        "Paleolítico",
-        "Idade dos Metais",
-        "Neolítico",
-        "Período da Pedra Polida"
-      ],
-      "Alternativas_Corretas": 1,
-    },
-    
-    {
-      "Pergunta": "Em qual local da Ásia o português é língua oficial?",
-      "Respostas": ["Índia", "Macau", "Filipinas", "Moçambique"],
-      "Alternativas_Corretas": 2,
-    },
+  /*{
+    "Pergunta": '',
+    "Respostas": ["", "", "", ""],
+    "Alternativas_Corretas": 0,
+  },*/
 ];
