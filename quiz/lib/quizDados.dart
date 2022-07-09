05a91108dcbcaaff70d60d1ad0e4779773ea70e3
projectsifma <ifmacaxiasprojects@gmail.com>
2022-07-09 13:36:21 -0300

    Junção de perguntas

--- quiz/lib/quizDados.dart
+++ quiz/lib/quizDados.dart
@@ -114,4 +114,75 @@ List quiz = [
     "Respostas": ["Grafite", "Rênio", "Ósmio", "Carbono"],
     "Alternativas_Corretas": 4,
   },
+  {
+    "Pergunta": "Qual é o plural de chapéu?",
+    "Respostas": ["chapéus", "chapeletas", "cartolas", "chapéis"],
+    "Alternativas_Corretas": 1,
+  },
+  {
+    "Pergunta": "Compatriota é aquele indivíduo que...",
+    "Respostas": [
+      "tem parentes no exterior",
+      "ama a pátria acima de tudo",
+      "se origina da mesma terra que outro",
+      "realiza compras em diferentes países"
+    ],
+    "Alternativas_Corretas": 3,
+  },
+  {
+    "Pergunta": 'Qual dos advérbios abaixo não existe?',
+    "Respostas": ["bastante", "quão", "menas", "tanto"],
+    "Alternativas_Corretas": 3,
+  },
+  {
+    "Pergunta": "Uma pessoa eclética é aquela que...",
+    "Respostas": [
+      "aprecia diferentes estilos, métodos, etc",
+      "gosta de apenas um tipo de música",
+      "viajou para vários países",
+      "tem medo de elevador"
+    ],
+    "Alternativas_Corretas": 1,
+  },
+  {
+    "Pergunta": "Quantas sílabas tem a palavra fisioterapeuta?",
+    "Respostas": ["cinco", "seis", "sete", "oito"],
+    "Alternativas_Corretas": 3,
+  },
+  {
+    "Pergunta":
+        'A primeira frase do hino brasileiro diz: "Ouviram do Ipiranga às margens plácidas". O que significa "plácidas"?',
+    "Respostas": ["frias", "calmas", "fundas", "rasas"],
+    "Alternativas_Corretas": 2,
+  },
+  {
+    "Pergunta": 'A palavra "volúpia" está relacionada com:',
+    "Respostas": ["luxúria", "doença", "pobreza", "nervosismo"],
+    "Alternativas_Corretas": 1,
+  },
+  {
+    "Pergunta": 'O verbo "aferir" está relacionado a:',
+    "Respostas": ["capacitar", "medir", "machucar", "manchar"],
+    "Alternativas_Corretas": 2,
+  },
+  {
+    "Pergunta": 'A palavra "latifúndio" está relacionada com:',
+    "Respostas": [
+      "coordenadas geográficas de um local",
+      "abrigo para cães",
+      "produtos de derivados do leite",
+      "grande área rural"
+    ],
+    "Alternativas_Corretas": 4,
+  },
+  {
+    "Pergunta": 'Incumbência significa...',
+    "Respostas": [
+      "descascar maçãs",
+      "conferir responsabilidade a alguém",
+      "interferir nos problemas dos outros",
+      "uma doença pulmonar"
+    ],
+    "Alternativas_Corretas": 2,
+  },
 ];
