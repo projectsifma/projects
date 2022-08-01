eaed7d59d1cfd7522240aa8746dd19287d73f08d
KayoVinicius <kayovinicius227@gmail.com>
2022-08-01 10:59:25 -0300

    Subindo algumas alterações

---
+++ quiz/lib/buttonQuiz.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class ButtonQuiz extends StatelessWidget {
+  final color;
+  final textColor;
+  final String buttonText;
+  final buttonTopped;
+  // ignore: use_key_in_widget_constructors
+  const ButtonQuiz({
+    this.buttonTopped,
+    this.color,
+    this.textColor,
+    required this.buttonText,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: buttonTopped,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(15),
+          child: Container(
+            alignment: Alignment.center,
+            height: 50,
+            width: double.infinity,
+            color: color,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child: Center(
+                  child: Text(
+                    buttonText,
+                    style: TextStyle(
+                        color: textColor,
+                        fontSize: 19,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ quiz/lib/myButton.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final color;
+  final textColor;
+  final String buttonText;
+  final buttonTopped;
+  // ignore: use_key_in_widget_constructors
+  const MyButton({
+    this.buttonTopped,
+    this.color,
+    this.textColor,
+    required this.buttonText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: buttonTopped,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ClipRRect(
+            borderRadius: BorderRadius.circular(30),
+            child: Container(
+              height: 50,
+              width: 300,
+              color: color,
+              child: Center(
+                  child: Text(
+                buttonText,
+                style: TextStyle(
+                    color: textColor,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700),
+              )),
+            )),
+      ),
+    );
+  }
+}
